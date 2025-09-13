b7de8445e711a255641012d0f95687b4cd2ab34c
Turis-Tv <turistv3@gmail.com>
2025-09-13 21:58:13 +0200

    Add files via upload

---
+++ 1-Iptv/6-Player/script.js
@@ -0,0 +1,291 @@
+//PlayerJs
+//vnd.apple.mpegurl
+    var player = new Playerjs({
+      id: "player",
+      file: [
+ 
+        {
+          title: "TRT 1",
+          poster: "",
+          type: "application/vnd.apple.mpegurl",
+          file: "https://tv-trt1.medya.trt.com.tr/master.m3u8"
+        },
+
+        {
+          title: "TRT 1",
+          poster: "",
+          type: "application/vnd.apple.mpegurl",
+          file: "https://trt.daioncdn.net/trt-1/master.m3u8?app=web"
+        },
+        {
+          title: "KANAL D",
+          poster: "",
+          type: "application/vnd.apple.mpegurl",
+          file: "https://demiroren-live.daioncdn.net/kanald/kanald.m3u8"
+        },
+
+        {
+          title: "KANAL D",
+          poster: "",
+          type: "application/vnd.apple.mpegurl",
+          file: "https://live.duhnet.tv/S2/HLS_LIVE/kanalddainp/playlist.m3u8"
+        },
+        {
+          title: "EU D",
+          poster: "",
+          type: "application/vnd.apple.mpegurl",
+          file: "https://live.duhnet.tv/S2/HLS_LIVE/eurodnp/playlist.m3u8"
+        },
+
+
+
+
+
+        {
+          title: "STAR TV",
+          poster: "",
+          type: "application/vnd.apple.mpegurl",
+          file: "https://dogus-live.daioncdn.net/startv/startv.m3u8"
+        },
+        {
+          title: "STAR TV",
+          poster: "",
+          type: "application/vnd.apple.mpegurl",
+          file: "http://dygvideo.dygdigital.com/live/hls/stardai?m3u8"
+        },
+        {
+          title: "EU STAR ",
+          poster: "",
+          type: "application/vnd.apple.mpegurl",
+          file: "https://raw.githubusercontent.com/ipstreet312/freeiptv/master/ressources/tur/estar.m3u8"
+        },
+
+
+
+
+
+
+        {
+          title: "SHOW TV",
+          poster: "",
+          type: "application/vnd.apple.mpegurl",
+          file: "https://ciner-live.daioncdn.net/showtv/showtv.m3u8"
+        },
+        {
+          title: "SHOW TV",
+          poster: "",
+          type: "application/vnd.apple.mpegurl",
+          file: "https://bloomberght-live.daioncdn.net/showtv/showtv.m3u8"
+        },
+
+        {
+          title: "Atv ",
+          poster: "",
+          type: "application/dash+xml",
+          file: "http://116.202.238.88/ATV_TR/index.m3u8"
+        },
+        {
+          title: "SHOW MAX ",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://raw.githubusercontent.com/ipstreet312/freeiptv/master/ressources/tur/shmax.m3u8"
+        },
+
+
+
+
+        {
+          title: "TV 8",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://tv8.daioncdn.net/tv8/tv8.m3u8?app=7ddc255a-ef47-4e81-ab14-c0e5f2949788"
+        }, 
+         {
+          title: "TV 8",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://tv8-live.daioncdn.net/tv8/tv8.m3u8"
+        }, 
+                {
+          title: " Tv8 | a",
+          poster: "",
+          type: "application/dash+xml",
+          file: "http://116.202.238.88/TV8_TR/index.m3u8"
+        },
+
+          {
+          title: "KANAL 7",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://kanal7-live.daioncdn.net/kanal7/kanal7.m3u8"
+        },
+          {
+          title: "KANAL 7 EU",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://livetv.radyotvonline.net/kanal7live/kanal7avr/playlist.m3u8"
+        },
+          {
+          title: "TGRT EU",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://tgrt-live.ercdn.net/tgrteu/tgrteu.m3u8"
+        },
+
+
+
+
+
+           {
+          title: "BEYAZ TV",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://beyaztv-live.daioncdn.net/beyaztv/beyaztv.m3u8"
+        },
+ 
+                  {
+          title: " 360 | a",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://trn03.tulix.tv/gt-sky360/index.m3u8 "
+        },
+
+                  {
+          title: " TEVE 2",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://demiroren.daioncdn.net/teve2/teve2.m3u8?app=6aab838a-437e-4a1b-bbd0-e30f79cdbbbd"
+        },
+                  {
+          title: " TRT BELGESEL",
+          poster: "",
+          type: "application/dash+xml",
+          file: "http://tv-trtbelgesel.medya.trt.com.tr/master.m3u8"
+        },
+                  {
+          title: " DMAX",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://dogus-live.daioncdn.net/dmax/dmax.m3u8"
+        },
+                  {
+          title: " TLC",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://dogus-live.daioncdn.net/tlc/tlc.m3u8"
+        },
+
+                {
+          title: "DOST TV  ",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://dost.stream.emsal.im/tv/live.m3u8"
+        },
+
+                {
+          title: "TV 4",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://turkmedya-live.ercdn.net/tv4/tv4.m3u8"
+        },
+                {
+          title: "TIVI 6",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://live.artidijitalmedya.com/artidijital_tivi6/tivi6/playlist.m3u8"
+        },
+                {
+          title: "CINE 1",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://live.artidijitalmedya.com/artidijital_cine1/cine1/playlist.m3u8"
+        },
+                {
+          title: "CINE 5",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://cdn-cine5tv.yayin.com.tr/cine5tv/cine5tv/playlist.m3u8"
+        },
+                {
+          title: "TÜRKİYE KLİNİKLERİ TV",
+          poster: "",
+          type: "application/dash+xml",
+          file: "http://mn-nl.mncdn.com/turkiyeklinikleri/smil:turkiyeklinikleri/chunklist_b3128000.m3u8"
+        },
+                {
+          title: "CGTN BELGESEL",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://mn-nl.mncdn.com/dogusdyg_drone/cgtn/playlist.m3u8"
+        },
+                {
+          title: "SAĞLIK CHANNEL",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://live.euromediacenter.com/saglikchannel/index.m3u8"
+        },
+                {
+          title: "MOR TV",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://live.euromediacenter.com/tvmor/index.m3u8"
+        },
+
+
+
+
+
+
+                {
+          title: "TRT ÇOCUK",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://tv-trtcocuk.medya.trt.com.tr/master.m3u8"
+        },
+                {
+          title: "TRT DİYANET ÇOCUK",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://tv-trtdiyanetcocuk.medya.trt.com.tr/master.m3u8"
+        },
+
+                {
+          title: "HT SPOR",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://ciner-live.ercdn.net/htspor/htspor.m3u8"
+        },
+                {
+          title: "SPORTS TV",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://live.sportstv.com.tr/hls/low/sportstv_hd/index.m3u8"
+        },
+
+                {
+          title: "TİVİ TURK",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://stream.tiviturk.de/live/tiviturk.m3u8"
+        },
+                {
+          title: "TİVİ TURK",
+          poster: "",
+          type: "application/dash+xml",
+          file: "http://nrttn172.kesintisizyayin.com:29010/nrttn/nrttn/playlist.m3u8"
+        },
+
+
+
+
+
+
+          
+          {
+          title: "ÜLKE TV",
+          poster: "",
+          type: "application/dash+xml",
+          file: "https://tgn.bozztv.com/dvrfl05/gt-ulketv/tracks-v1a1/mono.m3u8"
+        } 
+      ]
+    });
