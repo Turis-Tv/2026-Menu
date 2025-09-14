984286e4e16197b896a881fc5f01cf2718c328b7
Turis-Tv <turistv3@gmail.com>
2025-09-14 05:09:18 +0200

    Add files via upload

---
+++ 3-Mp3/1-Mp3-Playlist/01-Ugur-Isilak/script.js
@@ -0,0 +1,143 @@
+let mp3 = ["https://mp3kulisi.mobi/indir/Ferhat-Tunc/Ferhat-Tunc-Secmeler/Ferhat-Tunc-Ogul.mp3",
+"https://mp3kulisi.mobi/indir/Aydin-Agyer/Oy-Daglar-2018/Aydin-Agyer-Ogul.mp3",
+
+
+
+
+ 
+ "https://mp3kulisi.mobi/indir/seyda-karadeniz/vefa-2011/seyda-karadeniz-eyvah-ogul.mp3 ",
+ 
+" https://mp3kulisi.mobi/indir/Mustafa-Kucuk/Klasikleri/Mustafa-Kucuk-Yandim-Ogul.mp3 ",
+ 
+" https://mp3kulisi.mobi/indir/Nuray-Hafiftas/Eyvah-Gonul-1999/Nuray-Hafiftas-Sen-Kucuksun.mp3 ",
+ 
+ "https://mp3kulisi.mobi/indir/Izzet-Yildizhan/Birisi-1999/Izzet-Yildizhan-Ogul.mp3 ",
+ 
+ "https://mp3kulisi.mobi/indir/Ferhat-Tunc/Ferhat-Tunc-Secmeler/Ferhat-Tunc-Ogul.mp3 ",
+ 
+" https://mp3kulisi.mobi/indir/Erdal-Erzincan/Turkuler-Sevdamiz-Serisi/Erdal-Erzincan-Ismail-Ozden-Ogul.mp3 ",
+ 
+" https://mp3kulisi.mobi/indir/nazli-oksuz/yar-benimle-gelir-misin-2020/nazli-oksuz-ogul-gomdum-ogul-seni-topraga-gomdum.mp3 ",
+ 
+ "https://mp3kulisi.mobi/indir/Aydin-Agyer/Oy-Daglar-2018/Aydin-Agyer-Ogul.mp3 ",
+ 
+" https://mp3kulisi.mobi/indir/Ferhat-Tunc/Kavgamin-Cicegi-1999/Ferhat-Tunc-Gulmene-Ogul.mp3 ",
+ 
+" https://mp3kulisi.mobi/indir/Mustafa-Ugur/Bir-Tek-Canim-2011/Mustafa-Ugur-Ogul.mp3",
+ 
+" https://mp3kulisi.mobi/indir/Elif-Altintas/Unut-Beni-Senin-Hallerin-2013/Elif-Altintas-Ogul.mp3 ",
+ 
+ "https://mp3kulisi.mobi/indir/Hakki-Bulut/Senden-Vazgecmem-2012/Hakki-Bulut-Ogul.mp3 ",
+ 
+ "https://mp3kulisi.mobi/indir/Osman-Oztunc/Durmusun-Turkusu-1999/Osman-Oztunc-Ogul.mp3 ",
+ 
+" https://mp3kulisi.mobi/indir/Tulay-Maciran/Nerdesin-2014/Tulay-Maciran-Ogul.mp3 ",
+ 
+ "https://mp3kulisi.mobi/indir/Murat-Cobanoglu/Ozan-Murat-Sarkilari/Murat-Cobanoglu-Ogul-Babayi-Tanimaz.mp3 ",
+
+" https://mp3kulisi.mobi/indir/derinsu/derin-turkuler-2010/derinsu-can-ogul.mp3 ",
+
+ "https://mp3kulisi.mobi/indir/murat-guler/kilit-2009/murat-guler-ogul.mp3 ",
+
+ "https://mp3kulisi.mobi/indir/sevda-gul/sevdali-turkuler-2002/sevda-gul-ogul.mp3 ",
+
+ "https://mp3kulisi.mobi/indir/asik-hayrani/hisse-alsana-1987/asik-hayrani-ogul.mp3 ",
+" https://mp3kulisi.mobi/indir/abdullah-bektas/ogul-2016/abdullah-bektas-ogul.mp3 ",
+
+" https://mp3kulisi.mobi/indir/Sahin-Yucebas/Yoruldum-Anam-2017/Sahin-Yucebas-Ogul.mp3",
+
+ "https://mp3kulisi.mobi/indir/naki-yoksuli/dogma-bebek-2006/naki-yoksuli-ogul.mp3",
+ 
+" https://mp3kulisi.mobi/indir/cemali-kaya/yolcum-2016/cemali-kaya-ogul.mp3",
+ 
+" https://mp3kulisi.mobi/indir/Rusen-Aydeniz/Kara-Kiz-2019/Rusen-Aydeniz-Ogul.mp3",
+ 
+" https://mp3kulisi.mobi/indir/Cimen-Yalcin/Ogul-2020/Cimen-Yalcin-Ogul.mp3",
+ 
+ "https://mp3kulisi.mobi/indir/Arzu-Sahin/Ceylanim-1999/Arzu-Sahin-Sen-Kucuksun-Olemezsin.mp3",
+ 
+" https://mp3kulisi.mobi/indir/sibel-can/canim-benim-2001/sibel-can-yetim-yavrum.mp3",
+ 
+" https://mp3kulisi.mobi/indir/Ali-Turac/Ali-Turac-Klasikleri/Ali-Turac-Hele-Yavrum.mp3 ",
+ 
+ "https://mp3kulisi.mobi/indir/Seyfettin-Sucu/Anilarim-1980/Seyfettin-Sucu-Evlat-Acisi.mp3 ",
+ 
+" https://mp3kulisi.mobi/indir/Ayhan-Keser/Kavak-Yelleri-2018/Ayhan-Keser-Evlat-Acisi.mp3 ",
+ 
+ "https://mp3kulisi.mobi/indir/Ali-Ercan/Ali-Ercan-Uzelli-Arsiv/Ali-Ercan-Evlat-Acisi.mp3 ",
+ 
+ "https://mp3kulisi.mobi/indir/Ismail-Ozden/Gonul-Ezgilerimiz-1998/Ismail-Ozden-Sen-Kucuksun.mp3 ",
+ 
+" https://mp3kulisi.mobi/indir/Recebim/Vatan-2018/Recebim-Uyu-Bebegim.mp3 ",
+ 
+ 
+" https://mp3kulisi.mobi/indir/Ali-Haydar-Yildiz/Dinle-Ahvalimi-2019/Ali-Haydar-Yildiz-Emek-Cektim.mp3 ",
+ 
+ "https://mp3kulisi.mobi/indir/Cimen-Yalcin/Ogul-2020/Cimen-Yalcin-Ogul.mp3 ",
+ 
+ "https://mp3kulisi.mobi/indir/Cilgin-Sedat/Sirac-Bebegime-2007/Cilgin-Sedat-Sirac-Bebegim.mp3 ",
+ 
+ 
+ "https://mp3kulisi.mobi/indir/Metin-Ciftlik/Gunun-Birinde-2003/Metin-Ciftlik-Vur-Mezarci-Kazmayi.mp3 ",
+ 
+ 
+" https://mp3kulisi.mobi/indir/Bedirhan-Gokce/Adam-Kavgada-Belli-Olur-2007/Bedirhan-Gokce-Ogluma.mp3 ",
+ 
+ 
+ "https://mp3kulisi.mobi/indir/Ferhat-Tunc/Vurgunum-Hasretine-1986/Ferhat-Tunc-Uyu-Yavrum.mp3 "];
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let i = 0;
+function play(id){
+  let audioPlayer = document.getElementById('audio');
+  audioPlayer.src=mp3[id];
+  [...document.querySelectorAll(`.audio`)].forEach(v=>v.setAttribute("playing",false));
+  document.querySelector(`.audio-${id}`).setAttribute("playing",true);
+  audioPlayer.load();
+  audioPlayer.play();
+}
+function setup(){
+  let audioPlayer = document.getElementById('audio');
+  let playlist = document.getElementById('playlist');
+  mp3.forEach((v,i)=>{
+    playlist.innerHTML+=`
+      <li class="audio audio-${i}" onclick="play(${i})">${v.split("/")[5]}</li>
+    `;
+  });
+  play(i);
+  audioPlayer.pause();
+  audioPlayer.addEventListener('ended',()=>{
+    i++;
+    play(i);
+    if(i == mp3.length-1) i = 0;
+  }, false);
+}
+setup();
